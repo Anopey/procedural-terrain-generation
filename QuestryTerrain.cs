c1cfc222cebf8eeed29193d692f3d5ae548b8da9
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-20 11:02:19 +0300

    add tag addition

---
+++ QuestryTerrain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using QUnity.Utility;
+public class QuestryTerrain : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        //Add tags
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+        QEditorUtility.AddTag(tagManager, "Terrain");
+
+        gameObject.tag = "Terrain";
+    }
+
+}
