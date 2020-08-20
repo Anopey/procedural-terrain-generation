fbaf0120cceee189910545214236f98d9794f41e
Ozan Ürkmez <arkantos250@gmail.com>
2020-08-20 11:32:00 +0300

    basis for editor window

---
+++ QuestryTerrainEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(QuestryTerrain)), CanEditMultipleObjects]
+public class QuestryTerrainEditor : Editor
+{
+
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        QuestryTerrain questryTerrain = (QuestryTerrain)target;
+
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
